1c4c943174496277721b6bdd29d6c6a4c1cd03ca
Fattani Computers <amirraza2501@gmail.com>
2023-04-28 09:51:32 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Nav';
+import Home from './components/Home';
 
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        
-       
-       
-         
-      </header>
+    <Navbar />
+    <div className="content">
+      <Home />
     </div>
+  </div>
+   
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <h1>Aamir Akhter</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">Projects</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
